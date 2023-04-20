75a0b52e8196e1e997ae8e52ed82f2e6a1561c69
Muzzle886 <2568682437@qq.com>
2023-04-20 11:46:41 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const file = ref<File>()
+const fileInput = ref<HTMLInputElement>()
+const url = ref('')
+
+function fileChange(event: Event) {
+  console.log(url.value= URL.createObjectURL(fileInput.value?.files![0]));
+}
+</script>
+
+<template>
+  <input type="file" ref="fileInput" @change="fileChange" name="" id="" />
+  <video :src="url"></video>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
